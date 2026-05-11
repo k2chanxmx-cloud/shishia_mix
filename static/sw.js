290e0dbefeb46ac82300733ac76881f6927e7a11
k2chanxmx-cloud <k2chan.xmx@gmail.com>
2026-05-11 23:29:19 +0900

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,40 +1,10 @@
-const CACHE_NAME = "eighty-mix-history-v1";
-
-const urlsToCache = [
-    "/",
-    "/add",
-    "/search",
-    "/manifest.json"
-];
-
 self.addEventListener("install", function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
     self.skipWaiting();
 });
 
 self.addEventListener("activate", function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim();
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request);
-        })
-    );
 });
